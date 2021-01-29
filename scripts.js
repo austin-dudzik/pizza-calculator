7ee4594e0d04e2706df9fbdd01794466e0915c98
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2021-01-29 11:39:44 -0600

    Converted to jQuery

--- scripts.js
+++ scripts.js
@@ -1,30 +1,45 @@
+$(document).ready(
+
+  function() {
+
+    // Find the bill split button with jQuery
+
+    // Assign function calculatePizza to run when the button is clicked
+
+    $("#calculate").click(calculatePizza);
+
+  }
+
+
+);
+
 function calculatePizza() {
 
-// Get the number of toppings
-let toppings = prompt("How many toppings would you like on your pizza?");
+  // Get the number of toppings
+  //let toppings = prompt("How many toppings would you like on your pizza?");
 
-// Convert into number
-toppings = parseInt(toppings);
+  toppings = parseFloat($("input#toppings").val());
 
-// Get the number of people to split bill
-let people = prompt("How many people will be splitting the bill?");
+  // Get the number of people to split bill
+  //let people = prompt("How many people will be splitting the bill?");
 
-// Convert into number
-people = parseInt(people);
+  people = parseFloat($("input#people").val());
 
-// Create the basic pizza price ($15)
-let basicPrice = 15;
+  // Create the basic pizza price ($15)
+  let basicPrice = 15;
 
-// Calculate price for all toppings
-let top = toppings * 1.25;
+  // Calculate price for all toppings
+  let top = toppings * 1.25;
 
-// Add toppings price to pizza cost
-let pizzaWithToppings = basicPrice + top;
+  // Add toppings price to pizza cost
+  let pizzaWithToppings = basicPrice + top;
 
-// Divide pizza value to get split price
-let splitPrice = pizzaWithToppings / people;
+  // Divide pizza value to get split price
+  let splitPrice = pizzaWithToppings / people;
 
-// Display the pizza cost for each person
-alert(`The price that each person will need to pay is $${splitPrice.toFixed(2)}.`);
+  // Display the pizza cost for each person
+  //alert(`The price that each person will need to pay is $${splitPrice.toFixed(2)}.`);
+
+  $("#message").text(`The price that each person will need to pay is $${splitPrice.toFixed(2)}.`);
 
 }
